71bde40cee0e0fe0599e37fb3e5c6b1005661079
José Rocha <josetankian@yahoo.com>
2024-09-03 22:25:54 -0300

    Adicionei o boletimEstudantil.java e removi o AnatomiaJava.java.

---
+++ boletimEstudantil.java
@@ -0,0 +1,13 @@
+package josetankian;
+
+public class boletimEstudantil {
+	public static void main(String[] args) {
+		int mediaFinal = 5;
+		if(mediaFinal<6)
+			System.out.println("Reprovado");
+		else if(mediaFinal==6)
+			System.out.println("Prova Minerva");
+		else
+			System.out.println("Aprovado");
+	}
+}
